586bd547d8d97be4694628c03f9b56b657c72c03
marcosdeoliv <marcosdeoliveiramonteiro@gmail.com>
2023-07-01 16:43:35 -0300

    ajustes gerais

--- web-contas/src/main.tsx
+++ web-contas/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+import App from './App.tsx';
+
 import './index.css'
-import { Teste } from './assets/teste.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Teste />
+    <App />
   </React.StrictMode>,
 )
+
